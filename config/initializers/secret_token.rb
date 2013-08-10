be4c2073e032dbb9b68d5b9cab567df91b1ee889
Ben Hazelwood <ben@parkgrades.com>
2013-08-10 11:52:52 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AApp::Application.config.secret_token = 'cc69e88da4ae905fc76f4604df474a22cf875ca87a7facc30ace5319f2f0bd6164066ed4c2eb2e05d7892c1dec05f6dd8fab766ae21bd09f0c30f99ab54b8b85'
